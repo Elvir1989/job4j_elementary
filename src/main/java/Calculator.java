242a72bf59725aaea572cbbe85a20888ea1b11df
Elvir1989 <elvirpovar@gmail.com>
2021-12-14 11:43:05 +0300

    Calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int foutTimeTwo = four * two;
+        System.out.println(onePlusTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(foutTimeTwo);
+
+    }
+}
