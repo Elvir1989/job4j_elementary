a9f43bb5fcd1b07b5df3d4262b1330004aaa9094
Elvir1989 <elvirpovar@gmail.com>
2021-12-17 11:43:57 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/caculator/MathCalculator.java
+++ src/main/java/ru/job4j/caculator/MathCalculator.java
@@ -4,16 +4,16 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
 
-    public static double sumAndMultiply(double first, double second) { // метод скаладвает результаты двух методов
+    public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
     }
 
-    public static double sumTwoMethods(double first, double second) { // метод складвает результаты двух методов
+    public static double sumTwoMethods(double first, double second) {
         return difference(first, second) + division(first, second);
     }
 
-    public static double subtractionFourMethods(double first, double second) { // метод расчитываем сумму вычислеия всех четырех методов
+    public static double subtractionFourMethods(double first, double second) {
         return sum(first, second) - multiply(first, second) - difference(first, second) - division(first, second);
     }
 
