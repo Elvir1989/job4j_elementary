5410463269113bbfb720731092e17dd00f8e7aa8
Elvir1989 <elvirpovar@gmail.com>
2021-12-23 17:55:50 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,9 +4,13 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-                amount = (amount + amount * (percent * 0.1)) - salary;
+                amount = (amount + amount * (percent / 100)) - salary;
                 year++;
         }
         return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Mortgage.year(1000, 1200, 1));
     }
 }
