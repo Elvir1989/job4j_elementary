79bffbbf4ba2d8e1bd8ea54d732b5a1dd0805736
Elvir1989 <elvirpovar@gmail.com>
2023-02-04 00:27:39 +0300

    6.2.2. Переставить элементы массива

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -1,10 +1,12 @@
 package ru.job4j.array;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SwitchArrayTest {
+
     @Test
     public void whenSwap0to3() {
         int[] input = {1, 2, 3, 4};
@@ -12,7 +14,7 @@ public class SwitchArrayTest {
         int dest = input.length - 1;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {4, 2, 3, 1};
-        Assert.assertArrayEquals(expected, result);
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
@@ -22,7 +24,7 @@ public class SwitchArrayTest {
         int dest = input.length / 2;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {3, 2, 1, 4, 6};
-        Assert.assertArrayEquals(expected, result);
+        Assertions.assertArrayEquals(expected, result);
         }
 
     @Test
@@ -32,6 +34,6 @@ public class SwitchArrayTest {
         int dest = input.length / 2;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {1, 2, 6, 4, 3};
-        Assert.assertArrayEquals(expected, result);
+        Assertions.assertArrayEquals(expected, result);
     }
 }
